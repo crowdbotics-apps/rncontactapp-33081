4c875fd1f8e774a1d1f28ac3fb42debc6e63be0d
Crowdbotics <team@crowdbotics.com>
2022-01-23 06:39:37 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -1,12 +1,23 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from "react-native"
 
 export const globalOptions = {
   name: "rncontactapp_33081",
   url: "https://rncontactapp_33081.botics.co",
   api: "https://rncontactapp_33081.botics.co/api/v1"
 }
+
 export const modulesOptions = {
   "@modules/app-menu": {
-    "copy": "Routes available!"
+    copy: "Routes available!"
+  },
+  undefined: {
+    LOGO_URL: "",
+    BACKGROUND_URL: "",
+    HOME_SCREEN_NAME: "Home Screen",
+    validateEmail: "^[^\\s]+([.-]?\\w+)*@\\w+([.-]?\\w+)*(.\\w{2,3})+$",
+    SignInNavText: "Sign In",
+    SignUpNavText: "Sign Up",
+    SignInButtonText: "Login",
+    SignUpButtonText: "Sign Up"
   }
 }
